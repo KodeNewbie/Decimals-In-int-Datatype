bdbdcc3f9c13793df3787aac57ff08a29e3d3a4b
Saksham_Singh <mastersakshamsingh@gail.com>
2024-05-14 17:55:08 +0530

    first commit

---
+++ main.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+void main()
+{
+    int a = 5.0, b = 5.2, c = 5.4, d = 5.6, e = 5.8, f = 5.99 ;
+
+    printf("a = %d \n", a);
+    printf("b = %d \n", b);
+    printf("c = %d \n", c);
+    printf("d = %d \n", d);
+    printf("e = %d \n", e);
+    printf("f = %d \n", f);
+}
